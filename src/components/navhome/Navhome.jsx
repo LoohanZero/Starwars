fa43ffeb454f6760452f7df6bad1e3604347ca9a
Lujan Sanchez Cracco <lsanchez@visual-engin.com>
2022-11-15 22:05:49 +0100

    Styled with tailwind

--- src/components/navhome/Navhome.jsx
+++ src/components/navhome/Navhome.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { NavHomeLinkContainer, NavhomeLink } from './navhome_styles';
+import React from 'react';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 const Navhome = () => {
   return (
-    <>
-        <NavHomeLinkContainer className="nav-home-container">
-          <NavhomeLink 
-          className="nav-home-link"
-            to="/home" >HOME</NavhomeLink>
-          <NavhomeLink 
-          className="nav-home-link"
-          to="/starships">STARSHIPS</NavhomeLink>
-        </NavHomeLinkContainer>
-    </>
+        <header className="border border-solid border-zinc-800 border-x-0">
+          <nav className='flex items-center justify-center  h-12'>
+            <ul className='flex h-full'>
+              <li className="px-4 border border-zinc-800 h-full flex items-center border-y-0">
+                <Link to='/'>HOME</Link>
+              </li>
+              <li className="px-4 border border-zinc-800 h-full flex items-center border-y-0 border-l-0">
+                <Link to='/starships'>STARSHIPS</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
+import andor from '../../assets/img/1.jpeg';
+
+export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <div className="home-main flex items-center justify-center flex-col">
+        <img className='w-3/4' src={andor} alt='New film' />
+        <div className="w-3/4 home-texts my-12 text-justify font-semibold mx-10">
+          <p className="p-text py-2.5">
+            The crew behind Andor, now streaming on Disney+, was hard 
+            at work in production of the first season at Pinewood Studios 
+            at the time and Arjona and creator Tony Gilroy discussed Bix’s 
+            character arc at length. Now more than three quarters of the 
+            way through the season, viewers have witnessed the downward 
+            spiral taking Bix from self-made salvage dealer at the start to 
+            tortured captive being held against her will by the Empire. 
+            “Bix is bold and fearless, hardworking and incredibly loyal 
+            — sometimes to her own detriment,” Arjona explains. But her 
+            world begins to unravel swiftly.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Home
